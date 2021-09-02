5df68754f25cd6468db8b6e3da0a59532b2d17de
natclark <39421782+natclark@users.noreply.github.com>
2021-09-02 17:50:30 -0500

    add a few more fonts

--- src/lib/components/Details/index.svelte
+++ src/lib/components/Details/index.svelte
@@ -357,7 +357,11 @@
                     <div class="level">
                         <p class="normal">Font Family</p>
                         <select value={attributes.font_family} on:change={fontFamilyChange}>
+                            <option value="Segoe UI">Segoe UI</option>
                             <option value="Arial">Arial</option>
+                            <option value="Roboto">Roboto</option>
+                            <option value="Verdana">Verdana</option>
+                            <option value="sans-serif">sans-serif</option>
                         </select>
                     </div>
                     <div class="level">
